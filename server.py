1d1c70474ebd3bb130e60f2fd767b3fbe60911c0
Purinat33 <78997216+Purinat33@users.noreply.github.com>
2025-11-27 23:05:15 +0700

    Added a few more comments for ref

--- server.py
+++ server.py
@@ -4,8 +4,11 @@ from datetime import datetime
 
 def get(url='/'):
     if url == '/':
+        # / for index.html
         return build_response(200)
     else:
+        # Else we go with 404 for now
+        # In the future we might have other files/pages
         return build_response(404)
 
 
@@ -18,6 +21,11 @@ def parse_header(header: bytes):
     if 'GET' in lines[0]:
         targets = lines[0].split(' ')  # GET / HTTP/1.1
         return get(targets[1])  # /
+    # POST PUT DELETE etc. logic
+
+
+def parse_body(body: bytes):
+    pass
 
 
 def build_response(status):
@@ -31,7 +39,15 @@ def build_response(status):
         Content-Type: text/html; charset=utf-8
         Date: {day}, 
         """
-        return header.encode()
+    # Same thing as the get comments
+    # 201, 301 etc. etc. For now these two are suffice
+    else:
+        header = f"""HTTP/1.0 404 Not Found
+        """
+
+    # Maybe we only do that first line and connect to the rest of the header outside the if-else
+    # Need more study of headers
+    return header.encode()
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
