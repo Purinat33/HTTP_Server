07b9a5b7f1a9c431eb5ca9df53250fdfb1c3cfe3
Purinat33 <78997216+Purinat33@users.noreply.github.com>
2025-11-27 13:49:46 +0700

    Got basic connections. TODO: Headrs

--- client.py
+++ client.py
@@ -1,4 +1,16 @@
 from connection import *
 
+
+def build_request(url=HOST):
+    header = f"""GET / HTTP/1.1
+Host: {HOST}
+
+    
+    """
+
+    return header.encode()
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    s.sendall(build_request())
